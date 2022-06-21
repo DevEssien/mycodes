c1dc11554e0ee8676beef74c974948201402d5fc
Essien Emmanuel Samuel <100389523+DevEssien@users.noreply.github.com>
2022-06-21 22:35:45 +0100

    Update myFunctions.js

--- myFunctions.js
+++ myFunctions.js
@@ -3,17 +3,17 @@ function range(start, end) {
     const rangeList = [];
     for (let i = start; i <= end; i++) {
         rangeList.push(i);
-    };
+    }
     return rangeList;
-};
-//console.log(range(1,5))
+}
+//console.log(range(1,5));
 
 //range of random numbers from start to end exclusive
 function randrange(min, max) {
     let diff = Math.abs(max - min);
     let randint = Math.floor(Math.random() * diff) + min;
     return randint;
-};
+}
 //console.log(randrange(1,5));
 
 //array shuffle
@@ -24,7 +24,7 @@ function arrayShuf(arraY) {
         //let's shuffle the array by direct swapping
         rand = randrange(j, arrCopy.length);
         [arrCopy[j], arrCopy[rand]] = [arrCopy[rand], arrCopy[j]];
-    };
+    }
     return arrCopy;
-};
-console.log(arrayShuf(['a','b','c','d','e','f','g','h','i','j']))
+}
+console.log(arrayShuf(['a','b','c','d','e','f','g','h','i','j']));
