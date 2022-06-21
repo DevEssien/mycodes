ac045698cfdc4133980c716904bdf1be03309e00
Essien Emmanuel Samuel <essienemma300@gmail.com>
2022-06-21 22:42:29 +0100

    initial commit - added on 21st june,2022. 22:41

---
+++ leapYearChecker.js
@@ -0,0 +1,19 @@
+
+const year = 2020;
+
+if (year % 4 === 0) {
+    if (year % 100 ===0) {
+        if (year % 400 === 0) {
+            console.log('leap year');
+        }
+        else {
+            console.log('not a leap year');
+        }
+    }
+    else {
+        console.log('leap year');
+    }
+}
+else {
+    console.log('not a leap year');
+}
