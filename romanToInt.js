7a12216300f9a9067af629429cfe49a14e8d3e20
Essien Emmanuel Samuel <essienemma300@gmail.com>
2022-10-21 03:14:58 +0100

    leetcode roman to integer conversion

---
+++ romanToInt.js
@@ -0,0 +1,18 @@
+const romans = "XC";
+
+const standard = { M: 1000, D: 500, C: 100, L: 50, X: 10, V: 5, I: 1 };
+
+let int = 0;
+for (let index = 0; index <= romans.length - 1; index++) {
+    if (standard[romans[index + 1]] === undefined) {
+        int += standard[romans[index]];
+        break;
+    }
+    if (standard[romans[index]] >= standard[romans[index + 1]]) {
+        int += standard[romans[index]];
+    } else {
+        int -= standard[romans[index]];
+    }
+}
+
+console.log(int);
